5defdaff15a24675d826011a47c1b524df6f4abf
Usual <41396119+UsualCodeMan@users.noreply.github.com>
2022-07-27 21:53:48 -0700

    BigBall

--- UsualHub/BigPaintball.lua
+++ UsualHub/BigPaintball.lua
@@ -10,11 +10,9 @@ _G.Teamcheck = false
 
 spawn(function()
     local library = require(game:GetService("ReplicatedStorage").Framework.Library)
-    local env = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.Game["First Person Controller"])
-    
+    local env = getgenv(game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.Game["First Person Controller"])
     
     print("loaded");
-    --// Gun Bypass
     local old_fire = library.Network.Fire
     library.Network.Fire = newcclosure(function(self, ...)
     local args = {...}
@@ -30,20 +28,15 @@ spawn(function()
     env.DoesOwnGun = function(...)
     return (_G.unlock_all and true) or old_own(...)
     end
-    --
-    
-    --// Unlock All
+
     local old_own_gun = library.GunCmds.DoesOwnGun
     library.GunCmds.DoesOwnGun = newcclosure(function(self, ...)
     return (_G.unlock_all and true) or old_own_gun(self, ...)
     end)
-    --
     
-    --// Unlock Offsale
     for _, gun in next, library.Directory.Guns do
     gun["offsale"] = false
     end
-    --
 end)
 
 spawn(function()
