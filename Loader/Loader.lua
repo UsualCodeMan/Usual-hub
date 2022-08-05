94bcac40dcc86a079bf000ab61b5936d279e37a6
Usual <41396119+UsualCodeMan@users.noreply.github.com>
2022-08-05 13:14:09 -0700

    EEEEEEEEEEEEEEEEEEEEEEEE

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,8 +1,11 @@
 local Game = game.PlaceId
 local BigPaintBall = 3527629287
 local NenSim = 9555732501
+local Arsenal = 286090429
 if Game == BigPaintBall then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/BigPaintball.lua"))()
 elseif Game == NenSim then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Nen-Fighting-Simulator.lua"))()
+elseif Game == Arsenal then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Arsenal.lua"))()
 end
