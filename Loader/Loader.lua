8453991f798b64070144bc7a9be83538576a89fe
Usual <41396119+UsualCodeMan@users.noreply.github.com>
2022-08-18 20:56:45 -0700

    tap

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -2,10 +2,13 @@ local Game = game.PlaceId
 local BigPaintBall = 3527629287
 local NenSim = 9555732501
 local Arsenal = 286090429
+local TappingLegendsX = 8750997647
 if Game == BigPaintBall then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/BigPaintball.lua"))()
 elseif Game == NenSim then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Nen-Fighting-Simulator.lua"))()
 elseif Game == Arsenal then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Arsenal.lua"))()
+elseif Game == TappingLegendsX then
+    loadstring(game:HttpGet("https://github.com/UsualCodeMan/Usual-hub/blob/main/UsualHub/Tapping-Legends-X.lua"))()
 end
