4701ca1290692603a468572d379872e22cb1fa83
Usual <41396119+UsualCodeMan@users.noreply.github.com>
2026-02-24 23:21:47 -0800

    Updated tapping legends

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -10,5 +10,5 @@ elseif Game == NenSim then
 elseif Game == Arsenal then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Arsenal.lua"))()
 elseif Game == TappingLegendsX then
-    loadstring(game:HttpGet("https://github.com/UsualCodeMan/Usual-hub/blob/main/UsualHub/Tapping-Legends-X.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/UsualHub/Tapping-Legends-X.lua"))()
 end
