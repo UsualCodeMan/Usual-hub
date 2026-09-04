cb7b18b85cb924a34f74cdd1edc32cf1637f5fb8
Usual <41396119+UsualCodeMan@users.noreply.github.com>
2026-09-04 15:27:12 -0700

    Update Nen-Fighting-Simulator.lua

--- UsualHub/Nen-Fighting-Simulator.lua
+++ UsualHub/Nen-Fighting-Simulator.lua
@@ -20,7 +20,7 @@ end)
 
 -- Gui Setup
 local guiName = "UsualHub"
-local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/twink"))()
+local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/UsualCodeMan/Usual-hub/main/Loader/TwinkUI.Lua"))()
 local MainUI = UILibrary.Load(guiName)
 local Info = MainUI.AddPage("Info")
 local Farming = MainUI.AddPage("Farming")
